9c7d7142bd2db59bf533d92e999c1f15bf50b0d9
Bjorn Persmats <bjorn.pm@plankter.se>
2019-04-13 20:14:36 +0200

    Button for generating bunny (example)

--- Noah-s-ark/Assets/GameCode/ButtonThing.cs
+++ Noah-s-ark/Assets/GameCode/ButtonThing.cs
@@ -1,14 +1,29 @@
-﻿using UnityEngine;
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
 
 public class ButtonThing : MonoBehaviour
 {
     private void Start()
     {
-        Debug.Log("Lolwat start");
     }
 
     private void Update()
     {
-        Debug.Log("Lolwat update");
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Generating bunny");
+        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        Entity goal = entityManager.CreateEntity(Bootstrap.GoalArchetype);
+        entityManager.AddSharedComponentData(goal, Bootstrap.BunnyLook);
+        var goalPosition = new float3(15, 0, 5);
+
+        entityManager.SetComponentData(goal, new Scale { Value = new float3(50.0f, 50.0f, 50.0f) });
+        entityManager.SetComponentData(goal, new Position { Value = goalPosition });
+        entityManager.SetComponentData(goal, new Rotation { Value = quaternion.identity });
+        entityManager.SetComponentData(goal, new CircleComponent { Position = goalPosition, Radius = 5 });
     }
 }
