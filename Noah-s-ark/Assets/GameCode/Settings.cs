633aa4969567a2300e5d44b8afe0d559c0268bd8
Bjorn Persmats <bjorn.pm@plankter.se>
2019-04-14 15:01:25 +0200

    Camera bug fixed

--- Noah-s-ark/Assets/GameCode/Settings.cs
+++ Noah-s-ark/Assets/GameCode/Settings.cs
@@ -20,11 +20,12 @@ public class Settings : MonoBehaviour
     public float bulletSpeed = 10f;
     public float bulletLifeDuration = 3f;
 
-    public float cameraSpeed = 1f;
+    public float camPositionSpeed = 3.0f;
+    public float camRotationSpeed = 3.0f;
     public float cameraDistance = 25f;
     public float cameraHeight = 15f;
 
-    public bool debugMode = true;
+    public bool debugMode = false;
 
 
 }
